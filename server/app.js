c8321f2cfeb663e3a3ce0cf8426c4a1f7344e62a
parjoona <parjoona@hotmail.com>
2019-05-10 23:07:05 +0200

    setup base urls

--- server/app.js
+++ server/app.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const expressVue = require("express-vue")
+
+// Inits
 const app = express()
+const expressVueMiddleware = expressVue.init()
+
+// Middlewares
+app.use(expressVueMiddleware)
 
 const PORT = process.env.PORT || 5000
+
+// Routes
+app.use('/', require('./routes'))
+app.use('/users', require('./routes/users'))
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`)
