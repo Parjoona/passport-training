c423419843d753582a331bb3805e3e815ce8ac23
parjoona <parjoona@hotmail.com>
2019-05-16 09:20:13 +0200

    added some styling for welcome page

--- server/views/Welcome.vue
+++ server/views/Welcome.vue
@@ -1,5 +1,27 @@
 <template>
-  <h1>Welcome</h1>
+    <div class="mdc-layout-grid">
+      <div class="mdc-layout-grid__inner">
+        <div class="mdc-layout-grid__cell"></div>
+        <div class="mdc-layout-grid__cell">
+          <div class="mdc-card">
+            <div class="mdc-card__media card-title--body">
+                <i class="material-icons">lock</i>
+                <p class="mdc-typography--body1">Login or Register</p>
+            </div>
+
+            <div class="mdc-card__actions">
+              <button class="mdc-button mdc-card__action mdc-card__action--button">
+                <span class="mdc-button__label">Login</span>
+              </button>
+              <button class="mdc-button mdc-card__action mdc-card__action--button">
+                <span class="mdc-button__label">Register</span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="mdc-layout-grid__cell"></div>
+      </div>
+    </div>
 </template>
 
 
@@ -7,8 +29,15 @@
 export default {
   data() {
     return {
-      
+
     }
   }
 }
 </script>
+
+<style>
+  .card-title--body {
+    margin: 1.5rem 0 0;
+    text-align: center;
+  }
+</style>
